f66ca10bb3ce9763d01bd8ebdc5f17f552e8cefc
Samantha Finley <srfinley@uchicago.edu>
2020-03-27 11:58:58 -0400

    finds names fast

--- names/names.py
+++ names/names.py
@@ -1,5 +1,7 @@
 import time
 
+from binary_search_tree import BinarySearchTree
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -13,10 +15,23 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# original runtime: O(n^2) (technically O(n*m))
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+t = BinarySearchTree(names_1[0])
+for name in names_1:
+    t.insert(name)
+# insertion of n elements is O(nlogn)
+
+for name in names_2:
+    if t.contains(name):
+        duplicates.append(name)
+# assuming names_1 isn't in a hostile order, searching is O(logn) (technically theta)
+
+# runtime improvement on my computer: 9.1 seconds to 0.20 seconds
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
